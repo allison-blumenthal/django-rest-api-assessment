161f033fa9c3852833c59549100eb81cc9aa1c91
Allison Blumenthal <blumen.allison@gmail.com>
2023-06-05 23:40:15 -0400

    changed song model length field to duration field, created seed data for artist and genre

--- tunaapi/models/song.py
+++ tunaapi/models/song.py
@@ -7,4 +7,4 @@ class Song(models.Model):
   title = models.CharField(max_length=100)
   artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE)
   album = models.CharField(max_length=100)
-  length = models.IntegerField()
+  length = models.DurationField()
