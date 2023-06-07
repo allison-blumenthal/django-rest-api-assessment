a3ca0c6c2c4760c40455dea84c076546d5a18ab9
Allison Blumenthal <blumen.allison@gmail.com>
2023-06-06 20:04:36 -0400

    added view for genre

---
+++ tunaapi/views/genre.py
@@ -0,0 +1,41 @@
+"""View module for handling requests about songs"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from tunaapi.models import Genre
+
+
+
+class GenreView(ViewSet):
+    """Tuna api genre view"""
+    
+    def retrieve(self, request, pk):
+      """Handle GET requests for single genre
+      
+      Returns:
+        Response -- JSON serialized genre
+        """
+        
+      genre = Genre.objects.get(pk=pk)
+      serializer = GenreSerializer(song)
+      return Response(serializer.data)
+    
+    def list(self, request):
+      """Handle GET requests to get all genres
+      
+      Returns:
+          Response -- JSON serialized list of genres
+      """
+      
+      genres = Genre.objects.all()
+      serializer = GenreSerializer(genres, many=True)
+      return Response(serializer.data)
+    
+
+class GenreSerializer(serializers.ModelSerializer):
+  """JSON serializer for genres"""
+  
+  class Meta:
+      model = Genre
+      fields = ('id', 'description')
