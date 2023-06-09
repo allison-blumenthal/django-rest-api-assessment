bd2291a2f4860200495eda2fb2c8d1184bbdc83f
Allison Blumenthal <blumen.allison@gmail.com>
2023-06-09 17:08:12 -0400

    added related name to song model, added annotate method on artist inside retrieve function, added song_count inside artist serializer and additional fields in meta class for artist model

--- tunaapi/models/song.py
+++ tunaapi/models/song.py
@@ -5,6 +5,6 @@ from .artist import Artist
 class Song(models.Model):
   
   title = models.CharField(max_length=100)
-  artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE)
+  artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
   album = models.CharField(max_length=100)
   length = models.DurationField()
