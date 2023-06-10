2b805706efda9e7fb1135c3ce189530b8526bc6b
Allison Blumenthal <blumen.allison@gmail.com>
2023-06-10 11:30:29 -0400

    added serializer for songgenre

--- tunaapi/views/genre.py
+++ tunaapi/views/genre.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from tunaapi.models import Genre
+from tunaapi.models import Genre, SongGenre
 
 
 
@@ -18,8 +18,10 @@ class GenreView(ViewSet):
         """
       try: 
           genre = Genre.objects.get(pk=pk)
+          
           serializer = GenreSerializer(genre)
-          return Response(serializer.data)
+          return Response(serializer.data, status=status.HTTP_200_OK)
+        
       except Genre.DoesNotExist as ex:
           return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
     
@@ -68,11 +70,18 @@ class GenreView(ViewSet):
         genre = Genre.objects.get(pk=pk)
         genre.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
-
+      
+class SongGenreSerializer(serializers.ModelSerializer):
+  class Meta:
+      model = SongGenre
+      fields = ( 'song_id', )
+      depth = 1
+      
 class GenreSerializer(serializers.ModelSerializer):
   """JSON serializer for genres"""
+  songs = SongGenreSerializer(many=True, read_only=True)
   
   class Meta:
       model = Genre
-      fields = ('id', 'description')
-      depth = 1
+      fields = ('id', 'description', 'songs')
+      depth = 0
